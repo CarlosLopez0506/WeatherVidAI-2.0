a58652a3242e588e6b1e456035d7aa9787893e39
CarlosLopez0506 <esteban.sanchez4521@gmail.com>
2023-04-23 17:58:13 -0700

    New Main

---
+++ src/FileGetter.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FileGetter {
+    public static Multimedia[] files(String path) {
+
+        int fileCounter = 0;
+        Multimedia[] myFiles = new Multimedia[MyUtils.FileFinder(path).size()];
+//        myFiles[0].setDate("123","12");
+        for (String fileName : MyUtils.FileFinder(path)) {
+            myFiles[fileCounter] = new Multimedia(fileName);
+            ProcessBuilder processBuilder = new ProcessBuilder("./exiftool/exiftool", "-createDate", "-gpslatitude", "-gpslongitude", fileName);
+            try {
+                Process process = processBuilder.start();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    MyUtils.MetaHandler(myFiles[fileCounter],line);
+                    if (myFiles[fileCounter].location[0] != null && myFiles[fileCounter].location[1] != null){
+                        myFiles[fileCounter].setWeather(OpenWeather.GetDescription(OpenWeather.getWeatherData(Double.parseDouble(myFiles[fileCounter].location[0]), Double.parseDouble(myFiles[fileCounter].location[1]))));
+                    }
+                }
+                MyUtils.FileTypeDetector(myFiles[fileCounter]);
+                myFiles[fileCounter].getAllData();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            fileCounter += 1;
+        }
+        int n = myFiles.length;
+        Multimedia temp;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (myFiles[j].getDate() > myFiles[j+1].getDate()) {
+                    // swap files[j+1] and files[j]
+                    temp = myFiles[j];
+                    myFiles[j] = myFiles[j+1];
+                    myFiles[j+1] = temp;
+                }
+            }
+        }
+        return myFiles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,8 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println(OpenWeather.getWeatherData(40.5358, 20.203));
-        int fileCounter = 0;
-        Multimedia[] multimediaArray;
-        Multimedia[] myFiles = new Multimedia[MyUtils.FileFinder("/home/clopez543/Downloads").size()];
-//        myFiles[0].setDate("123","12");
-        for (String fileName : MyUtils.FileFinder("/home/clopez543/Downloads")) {
-            System.out.println("/////////////////////" + fileCounter);
-            myFiles[fileCounter] = new Multimedia(fileName);
-            ProcessBuilder processBuilder = new ProcessBuilder("./exiftool/exiftool", "-createDate", "-gpslatitude", "-gpslongitude", fileName);
-            try {
-                Process process = processBuilder.start();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                String line;
-//                System.out.println("File #: " + fileCounter + "\n");
-                while ((line = reader.readLine()) != null) {
-                    MyUtils.MetaHandler(myFiles[fileCounter],line);
-                    if (myFiles[fileCounter].location[0] != null && myFiles[fileCounter].location[1] != null){
-                        myFiles[fileCounter].setWeather(OpenWeather.GetDescription(OpenWeather.getWeatherData(Double.parseDouble(myFiles[fileCounter].location[0]), Double.parseDouble(myFiles[fileCounter].location[1]))));
-                    }
-                }
-                MyUtils.FileTypeDetector(myFiles[fileCounter]);
-                myFiles[fileCounter].getAllData();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            fileCounter += 1;
-        }
+        System.out.println(Arrays.toString(FileGetter.files("/home/clopez543/Downloads")));
     }
+
 }
