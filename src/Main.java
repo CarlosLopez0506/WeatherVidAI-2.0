66a9ef7bf77cadc0d7fb713877c9e80be3d69c5a
CarlosLopez0506 <esteban.sanchez4521@gmail.com>
2023-04-24 17:18:09 -0700

    Modified .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 import java.util.Arrays;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String absolutePath = MenuCaller.openFileChooser();
-        String directoryName = absolutePath + "/HopeYourFolderIsOkay";
-        Multimedia[] myFiles = FileGetter.files(absolutePath);
-        MyApi.mapQuestApi(myFiles[0].getLocation(),myFiles[myFiles.length-1].getLocation(),absolutePath);
-        FileManager.folderRemover(directoryName);
-        FileManager.folderCreator(directoryName);
+        String openaiKey = System.getenv("OPENAI_KEY");
+        System.out.println(openaiKey);
 
-        myFiles[myFiles.length-1].getAllData();
-        for (Multimedia type : myFiles) {
-            if (type.getImage() == null) {
-                System.out.println(" The value is null\n\n");
-            } else if (type.getImage()) {
-                String newFilePath = FileManager.toMp4(type.getPath());
-                FFMPEGCommands.photoToVideo(type.getPath(), newFilePath);
-                type.setPath(newFilePath);
-            } else {
-                System.out.println(" The value is false in\n\n");
-            }
-            type.getAllData();
-        }
+//        String absolutePath = MenuCaller.openFileChooser();
+//        String directoryName = absolutePath + "/HopeYourFolderIsOkay";
+//        FileManager.folderRemover(directoryName);
+//        FileManager.folderCreator(directoryName);
+//        Multimedia[] myFiles = FileGetter.files(absolutePath);
+//        MyApi.mapQuestApi(myFiles[0].getLocation(),myFiles[myFiles.length-1].getLocation(),absolutePath);
+//
+//        myFiles[myFiles.length-1].getAllData();
+//        for (Multimedia type : myFiles) {
+//            if (type.getImage() == null) {
+//                System.out.println("The file extension wasn't identified so it will not to be in the video\n\n");
+//            } else if (type.getImage()) {
+//                String newFilePath = FileManager.toMp4(type.getPath());
+//                FFMPEGCommands.photoToVideo(type.getPath(), newFilePath);
+//                type.setPath(newFilePath);
+//            } else {
+//                System.out.println("This is not a video\n\n");
+//            }
+//            type.getAllData();
+//        }
     }
 }
 
